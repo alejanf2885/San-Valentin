bb85fbebf36460139afa9a37a97e6a634d0f65f8
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-02-05 20:13:58 +0100

    fix

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ noButton.addEventListener("mouseover", () => {
     noButton.style.position = "absolute";
     noButton.style.left = `${x}px`;
     noButton.style.top = `${y}px`;
+})
+
+noButton.addEventListener("click", () => {
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
+    noButton.style.position = "absolute";
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
 });
 
 
